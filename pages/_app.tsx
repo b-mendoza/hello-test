56d4f0623a418d30f6e73e2f9b8c290657b5f72f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-08-20 20:09:01 -0600

    feat: update initial boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { GlobalStyles } from 'styles/globals'
+
+function __NextApp(props: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+
+      <props.Component {...props.pageProps} />
+    </>
+  )
 }
-export default MyApp
+
+export default __NextApp
